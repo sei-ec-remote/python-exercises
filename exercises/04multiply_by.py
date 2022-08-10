98cf20a813a5df2323f558dd9e5190b5f4f3de7a
cjones1047 <cjmj1047@gmail.com>
2022-08-10 15:07:00 -0500

    04 done

--- exercises/04multiply_by.py
+++ exercises/04multiply_by.py
@@ -4,9 +4,16 @@
 # You'll want to apply your fundamental programming knowledge here.
 # What are the pieces to this problem? You'll need to define a function,
 # need a return statement, need a for loop to iterate over the array.
-#
+
+def multiply_by(list, number):
+    edited_list = [this_num * number for this_num in list]
+    return print(edited_list)
+
+
 # Example function call:
 #
-# multiply_by([1, 2, 3], 5)
+multiply_by([1, 2, 3], 5)
 #
 # > [5, 10, 15]
+
+multiply_by([3, 6, 9], 5)
