1e20cd26ca68e1ffc5a75b4a04c20686937af360
Alys <alice.g.cooper@gmail.com>
2022-08-10 15:43:50 -0400

    finish exercises 4 and 5

--- exercises/05factorial.py
+++ exercises/05factorial.py
@@ -9,3 +9,16 @@
 #
 # > 120
 #
+def factorial(n):
+    factorial_list = []
+    result = 1
+    for i in range(1, n+1):
+        factorial_list.append(i)
+    for number in factorial_list:
+        result = number * result
+        # don't need to return it - it does so already
+        # print(result) 
+    # print(factorial_list)
+    print(result)
+
+factorial(5)
